fae78a1719d810a3022a4dba53026ccf1ecc81e4
dominicod <dominicodonnell99@gmail.com>
2022-09-23 13:50:24 -0500

    Feat: create controller logic as well as data for bulk discounts in seeds

--- app/controllers/merchant/bulk_discounts_controller.rb
+++ app/controllers/merchant/bulk_discounts_controller.rb
@@ -1,2 +1,9 @@
 class Merchant::BulkDiscountsController < ApplicationController
+  def index
+    @bulk_discounts = Merchant.find(params[:merchant_id]).bulk_discounts
+  end
+
+  def show
+    @bulk_discount = BulkDiscount.find(params[:id])
+  end
 end
