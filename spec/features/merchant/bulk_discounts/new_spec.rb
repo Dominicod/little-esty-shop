c55f937f6e2fa3c7d9b4b6d2462cda87f67d375c
dominicod <dominicodonnell99@gmail.com>
2022-09-23 15:40:41 -0500

    Feat: logic created for creating a bulk discount

--- spec/features/merchant/bulk_discounts/new_spec.rb
+++ spec/features/merchant/bulk_discounts/new_spec.rb
@@ -13,41 +13,45 @@ RSpec.describe "merchant bulk discounts new page", type: :feature do
         expect(page).to have_link("Create Discount")
         click_on "Create Discount"
       end
-      expect(page.current_path).to eq new_merchant_bulk_discount_path
+      expect(page.current_path).to eq new_merchant_bulk_discount_path(1)
 
       within("#new_bulk_discount") do
-        expect(page).to have_field("percentage")
-        expect(page).to have_field("quantity_threshold")
-        fill_in "percentage", with: "50%"
-        fill_in "quantity_threshold", with: "20"
-        click_on "Create new bulk discount"
+        expect(page).to have_field("bulk_discount_percentage")
+        expect(page).to have_field("bulk_discount_quantity_threshold")
+        fill_in "bulk_discount_percentage", with: "50%"
+        fill_in "bulk_discount_quantity_threshold", with: "20"
+        click_on "Create Bulk discount"
       end
       expect(page.current_path).to eq merchant_bulk_discounts_path(1)
 
       within("#bulk_discounts") do
-        within("#discount-1") do
+        within("#discount-#{BulkDiscount.last.id}") do
           expect(page).to have_content("50%")
           expect(page).to have_content("20")
-          expect(page).to have_link("Bulk_Discount #1")
+          expect(page).to have_link("Bulk_Discount ##{BulkDiscount.last.id}")
         end
+      end
+
+      within("#flash_message") do
+        expect(page).to have_content("Bulk Discount created successfully")
       end
     end
 
     it "If I attempt to create a new discount and do not fill in the correct information
         I am redirected back to the new page and I am informed via flash message that I entered
         incorrect information." do
-      visit new_merchant_bulk_discount_path
+      visit new_merchant_bulk_discount_path(1)
 
       within("#new_bulk_discount") do
-        expect(page).to have_field("percentage")
-        expect(page).to have_field("quantity_threshold")
-        fill_in "percentage", with: "50%"
-        click_on "Create new bulk discount"
+        expect(page).to have_field("bulk_discount_percentage")
+        expect(page).to have_field("bulk_discount_quantity_threshold")
+        fill_in "bulk_discount_percentage", with: "50%"
+        click_on "Create Bulk discount"
       end
-      expect(page.current_path).to eq new_merchant_bulk_discount_path
+      expect(page.current_path).to eq new_merchant_bulk_discount_path(1)
 
-      within("#flash-message") do
-        expect(page).to have_content("Bulk-Discount not created: Missing required information")
+      within("#flash_message") do
+        expect(page).to have_content("Bulk Discount not created: Missing required information")
       end
     end
   end
