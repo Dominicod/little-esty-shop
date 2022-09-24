1c8ff9541c43caf26b3603e338c57f29d0bf6c7a
dominicod <dominicodonnell99@gmail.com>
2022-09-24 13:39:00 -0500

    Test: create test skeleton, Refactor: changed to let blocks to call for invoice and merchant

--- spec/features/merchant/invoices/show_spec.rb
+++ spec/features/merchant/invoices/show_spec.rb
@@ -1,16 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Invoices Show Page' do
+  let(:merchant) { Merchant.first }
+  let(:invoice_1) { merchant.invoices.first }
   before(:each) { mock_api_call }
 
   describe 'As a merchant' do
     describe 'When I visit my merchant invoices show page' do
       it 'Then I see information related to that invoice' do
-        @merchant = Merchant.first
-        invoice_1 = @merchant.invoices.first
-        invoice_2 = @merchant.invoices.last
-
-        visit merchant_invoice_path(@merchant, invoice_1)
+        visit merchant_invoice_path(merchant, invoice_1)
 
           expect(page).to have_content("Invoice ID: 1")
           expect(page).to have_content("Invoice Status: cancelled")
@@ -20,10 +18,7 @@ RSpec.describe 'Merchant Invoices Show Page' do
       end
 
       it 'I see all of my items on the invoice including the name' do
-        @merchant = Merchant.first
-        invoice_1 = @merchant.invoices.first
-
-        visit merchant_invoice_path(@merchant, invoice_1)
+        visit merchant_invoice_path(merchant, invoice_1)
 
         within "div#1" do
           expect(page).to have_content("Item Name: Item Qui Esse")
@@ -36,20 +31,12 @@ RSpec.describe 'Merchant Invoices Show Page' do
       end
 
       it 'I see the total revenue that will be generated from all of my items on the invoice' do
-        @merchant = Merchant.first
-        invoice_1 = @merchant.invoices.first
-        invoice_2 = @merchant.invoices.last
-
-        visit merchant_invoice_path(@merchant, invoice_1)
+        visit merchant_invoice_path(merchant, invoice_1)
           expect(page).to have_content("Total Revenue: $21067.77")
       end
 
       it 'I see that each invoice item status is a select field and I see that the invoice items current status is selected' do
-        @merchant = Merchant.first
-        invoice_1 = @merchant.invoices.first
-        invoice_2 = @merchant.invoices.last
-
-        visit merchant_invoice_path(@merchant, invoice_1)
+        visit merchant_invoice_path(merchant, invoice_1)
 
         within "div#1" do
           within "#status_#{invoice_1.invoice_items.first.id}" do
@@ -60,11 +47,7 @@ RSpec.describe 'Merchant Invoices Show Page' do
 
       describe 'When I click this select field' do
         it 'I can select a new status for the Item' do
-          @merchant = Merchant.first
-          invoice_1 = @merchant.invoices.first
-          invoice_2 = @merchant.invoices.last
-
-          visit merchant_invoice_path(@merchant, invoice_1)
+          visit merchant_invoice_path(merchant, invoice_1)
 
           within "div#1" do
             within "#status_#{invoice_1.invoice_items.first.id}" do
@@ -76,11 +59,7 @@ RSpec.describe 'Merchant Invoices Show Page' do
         end
 
         it 'next to the select field I see a button to "Update Item Status"' do
-          @merchant = Merchant.first
-          invoice_1 = @merchant.invoices.first
-          invoice_2 = @merchant.invoices.last
-
-          visit merchant_invoice_path(@merchant, invoice_1)
+          visit merchant_invoice_path(merchant, invoice_1)
 
           within "div#1" do
             within "#status_#{invoice_1.invoice_items.first.id}" do
@@ -91,27 +70,19 @@ RSpec.describe 'Merchant Invoices Show Page' do
 
         describe 'When I click this button' do
           it 'I am taken back to the merchant invoice show page' do
-            @merchant = Merchant.first
-            invoice_1 = @merchant.invoices.first
-            invoice_2 = @merchant.invoices.last
-
-            visit merchant_invoice_path(@merchant, invoice_1)
+            visit merchant_invoice_path(merchant, invoice_1)
 
             within "div#1" do
               within "#status_#{invoice_1.invoice_items.first.id}" do
                 select :shipped, from: 'status'
                 click_button 'Update Item Status'
-                expect(current_path).to eq(merchant_invoice_path(@merchant, invoice_1))
+                expect(current_path).to eq(merchant_invoice_path(merchant, invoice_1))
               end
             end
           end
 
           it 'I see that my Items status has now been updated' do
-            @merchant = Merchant.first
-            invoice_1 = @merchant.invoices.first
-            invoice_2 = @merchant.invoices.last
-
-            visit merchant_invoice_path(@merchant, invoice_1)
+            visit merchant_invoice_path(merchant, invoice_1)
 
             within "div#1" do
               within "#status_#{invoice_1.invoice_items.first.id}" do
@@ -123,10 +94,14 @@ RSpec.describe 'Merchant Invoices Show Page' do
           end
 
           it 'I see the total revenue for my merchant from this invoice (not including discounts)' do
+            visit merchant_invoice_path(merchant, invoice_1)
+
 
           end
 
           it 'I see the total discounted revenue for my merchant from this invoice which includes bulk discounts in the calculation' do
+            visit merchant_invoice_path(merchant, invoice_1)
+
 
           end
         end
