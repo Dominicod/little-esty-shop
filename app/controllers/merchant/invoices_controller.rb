81177fe8955693fe90016c153dbb1546af6a67cd
dominicod <dominicodonnell99@gmail.com>
2022-09-25 13:43:45 -0500

    Mock: Mock up model methods for testing, fix bug where all invoice items are shown on the invoice rather than the merchants

--- app/controllers/merchant/invoices_controller.rb
+++ app/controllers/merchant/invoices_controller.rb
@@ -8,12 +8,16 @@ class Merchant::InvoicesController < ApplicationController
   def show
     @invoice = Invoice.find(params[:id])
     @merchant = Merchant.find(params[:merchant_id])
-    @invoice_items = @invoice.invoice_items.each do |invoice_item|
-      if invoice_item.discountable?
-        binding.pry
-        invoice_item.discount
-      else
-        invoice_item
+    @invoice_items = []
+    @invoice.invoice_items.each do |invoice_item|
+      if owned_by_current_merchant?(invoice_item, @merchant)
+        @invoice_items << invoice_item
+        # if invoice_item.discountable?
+        #   BulkDiscount.max_percentage(invoice_item.id, @merchant.id)
+        #   # binding.pry
+        # else
+        #   invoice_item
+        # end
       end
     end
   end
@@ -27,6 +31,10 @@ class Merchant::InvoicesController < ApplicationController
   end
 
   private
+  def owned_by_current_merchant?(invoice_item, merchant)
+    invoice_item.item.merchant == merchant
+  end
+
   def invoice_item_params
     params.permit(:invoice_id, :item_id, :quantity, :unit_price, :status)
   end
--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,12 @@ class BulkDiscount < ApplicationRecord
 
   validates :percentage, presence: true
   validates :quantity_threshold, presence: true
+
+  def self.max_percentage(invoice_id, merchant_id)
+      joins(merchant: [items: :invoice_items])
+      .where('invoice_items.id = ? AND merchants.id = ? AND invoice_items.quantity >= quantity_threshold', invoice_id, merchant_id)
+      .distinct
+      .order(percentage: :desc)
+      .limit(1)
+  end
 end
